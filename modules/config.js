a1daf6357189358e3f5d855bd6ebe5618c2ef577
Peyman Khalili Zonouz <p.zonouz@hotmail.com>
2020-12-11 11:49:07 +0330

    Revert "Delete config.js"
    This reverts commit 09908d9cf3eba388eb6b2af7f9c775d858a9aed1.

---
+++ modules/config.js
@@ -0,0 +1,13 @@
+const PORT = 5000;
+const LOCAL_DATABASE = "mongodb://127.0.0.1:27017/carRepairCenter";
+const GMAIL_USERNAME = "p.zonouz@gmail.com";
+const GMAIL_PASSWORD = "peyman1363";
+const DELAY_CRITERIA = 1; // days
+
+module.exports = {
+  PORT,
+  LOCAL_DATABASE,
+  GMAIL_USERNAME,
+  GMAIL_PASSWORD,
+  DELAY_CRITERIA,
+};
