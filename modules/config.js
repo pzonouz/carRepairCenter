67ab4ba5a7f60aa72f15b565b8a3368a8f6ef13f
Peyman Khalili Zonouz <p.zonouz@hotmail.com>
2020-12-11 11:40:08 +0330

    Working...add config file to igonre list and delete from repo. going to deploy on heroku and mongodb atlas.

--- modules/config.js
+++ modules/config.js
@@ -1,5 +1,10 @@
-const PORT = 5000;
-const LOCAL_DATABASE = "mongodb://127.0.0.1:27017/carRepairCenter";
+let { PORT } = process.env;
+if (PORT == null || PORT == "") {
+  PORT = 5000;
+}
+// const LOCAL_DATABASE = "mongodb://127.0.0.1:27017/carRepairCenter";
+const MONGODB_COMPASS =
+  "mongodb+srv://peyman:<password>@cluster0.nf4ub.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const GMAIL_USERNAME = "p.zonouz@gmail.com";
 const GMAIL_PASSWORD = "peyman1363";
 const DELAY_CRITERIA = 1; // days
